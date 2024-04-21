94352465858c06d8cf0a92d3f7228b21a666f76e
Ivan Kysiel <keselivdaf@gmail.com>
2024-04-21 15:08:08 +0200

    hw4

--- hw4/hw4.3/userbio.js
+++ hw4/hw4.3/userbio.js
@@ -2,11 +2,15 @@ let userAge = prompt("Скільки тобі років?");
 let userCity = prompt("В якому місті ти проживаєш?");
 let userFavoriteSport = prompt("Який твій улюблений вид спорту");
 
-const City = ["Київ", "Лондон", "Вашингтон"];
-let cityСoincided =
-  City[0] === userCity || City[1] === userCity || City[2] === userCity;
-if (!cityСoincided) {
+const city = ["Київ", "Лондон", "Вашингтон"];
+const country = ["Україна", "Велика Британія", "Америка"];
+
+let isCapital = city.includes(userCity);
+
+if (!isCapital) {
   alert(`Тобі ${userAge} років. Ти живеш в місті ${userCity}`);
 } else {
-  alert(`Тобі ${userAge} років. Ти живеш у столиці ${userCity}`);
+  let index = city.indexOf(userCity);
+  let capital = country[index];
+  alert(`Тобі ${userAge} років. Ти живеш у столиці ${capital}`);
 }
